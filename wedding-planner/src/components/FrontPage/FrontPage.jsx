2494a3018448cac04a6aa33bced9256091a09514
ryougi1702 <wangpohshan@gmail.com>
2023-03-21 20:52:05 +0800

    forgot to add components/FrontPage

---
+++ wedding-planner/src/components/FrontPage/FrontPage.jsx
@@ -0,0 +1,63 @@
+import Container from "react-bootstrap/esm/Container";
+import Footer from "../Footer/Footer";
+import Sample from "../../Sample.jsx";
+import HeartyNavbar from "../HeartyNavbar/HeartyNavbar"
+
+
+export default function FrontPage() {
+    return (
+        <Container id="appContainer" fluid>
+            <Container
+                id="mainContainer"
+                style={{ padding: 0, background: "var(--bs-gray)", "min-height": "100vh" }}
+            >
+                <HeartyNavbar id="navbar" />
+                <Container id="bodyContainer">
+                    <p>foobar</p>
+                    <br /> <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br /> <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <p>foobar</p>
+                </Container>
+
+                <Footer id="footer" />
+            </Container>
+            <Sample></Sample>
+        </Container>
+    );
+}
