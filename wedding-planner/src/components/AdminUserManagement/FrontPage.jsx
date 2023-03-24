c7179e3f812d74ff2c5ea1791047dcd18f5e0aac
ryougi1702 <wangpohshan@gmail.com>
2023-03-25 01:33:14 +0800

    test

--- wedding-planner/src/components/AdminUserManagement/FrontPage.jsx
+++ wedding-planner/src/components/AdminUserManagement/FrontPage.jsx
@@ -1,13 +1,18 @@
+import { useEffect } from "react";
 import HeartyNavbar from "../HeartyNavbar/HeartyNavbar";
 
-export default function FrontPage() {
+export default function AdminUserManagement() {
+    
+    // empty array so useEffect to set doc title only runs once
+    useEffect(() => document.title = "Admin: User Management", []);
+    
     // note that HeartyNavbar has an id specified in its component jsx file
     return (
         <div id="appContainer">
             <HeartyNavbar /> 
             <div id="bodyContainer">
                 <div className="bodyTextColumn">
-                    <p>foobar</p>
+                    <p>admin user stuff</p>
                 </div>
             </div>
 
