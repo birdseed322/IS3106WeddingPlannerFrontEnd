14781c83fc0d18555acb74510a453cad019d6442
Josephine <josephinesumargo88@gmail.com>
2023-03-26 22:17:44 +0800

    resolved merge conflicts

--- wedding-planner/src/routes/EndPoints.jsx
+++ wedding-planner/src/routes/EndPoints.jsx
@@ -9,6 +9,7 @@ import VendorDetailpage from '../components/VendorManagement/VendorDetailPage.js
 import CategoryDisplayPage from '../components/VendorManagement/CategoryDisplayPage.jsx';
 import AdminUserManagement from '../components/AdminUserManagement/AdminUserManagement.jsx';
 import GuestList from '../components/GuestManagement/GuestList.jsx';
+import WeddingChecklist from '../components/LogisticsManagement/WeddingChecklist.jsx';
 // Component to handle routing. Take note of the format of the pathing and how to add a Route (url endpoint). Login component is created as an example. 
 
 function EndPoints() {
@@ -34,6 +35,7 @@ function EndPoints() {
                 <Route exact path="/sample" element={<Sample />}/>
                 <Route exact path="/vendor/request" element={<VendorRequest/>}/>
                 <Route exact path="/AdminUserManagement" element={<AdminUserManagement />}/>
+                <Route exact path="/LogisticsManagement/WeddingChecklist" element={<WeddingChecklist />} />
                 <Route exact path="/VendorSearchPage" element={<SearchPage/>}/>
                 <Route path="/VendorSearchPage/VendorName/:vendorName" element={<VendorDetailpage/>}/>
                 <Route path="/VendorSearchPage/Category/:vendorCategory" element={<CategoryDisplayPage/>}/>
