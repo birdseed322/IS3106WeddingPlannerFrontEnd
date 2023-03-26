f79094775c89af1aca274a12b342b7ad27751c2c
Josephine <josephinesumargo88@gmail.com>
2023-03-26 22:04:45 +0800

    renamed WeddingChecklist.jsx

--- wedding-planner/src/routes/EndPoints.jsx
+++ wedding-planner/src/routes/EndPoints.jsx
@@ -9,54 +9,62 @@ import VendorDetailpage from "../components/VendorManagement/VendorDetailPage.js
 import CategoryDisplayPage from "../components/VendorManagement/CategoryDisplayPage.jsx";
 import AdminUserManagement from "../components/AdminUserManagement/AdminUserManagement.jsx";
 import GuestList from "../components/GuestManagement/GuestList.jsx";
-import WeddingChecklist from "../components/LogisticsManagement/WeddingCheclist.jsx";
+import WeddingChecklist from "../components/LogisticsManagement/WeddingChecklist.jsx";
 // Component to handle routing. Take note of the format of the pathing and how to add a Route (url endpoint). Login component is created as an example.
 
 function EndPoints() {
-  // FOR IMPLEMENTING LOGIN: work in progress!!
-  // const [token, setToken] = useState();
+    // FOR IMPLEMENTING LOGIN: work in progress!!
+    // const [token, setToken] = useState();
 
-  // if(!token) {
-  //     return(
-  //     <BrowserRouter>
-  //         <Routes>
-  //             <Route exact path="/"  element={<FrontPage />}/>
-  //             <Route exact path="/login" element={<Login setToken={setToken} />} />
-  //         </Routes>
-  //     </BrowserRouter>)
-  //   }
+    // if(!token) {
+    //     return(
+    //     <BrowserRouter>
+    //         <Routes>
+    //             <Route exact path="/"  element={<FrontPage />}/>
+    //             <Route exact path="/login" element={<Login setToken={setToken} />} />
+    //         </Routes>
+    //     </BrowserRouter>)
+    //   }
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<FrontPage />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/sample" element={<Sample />} />
-          <Route exact path="/vendor/request" element={<VendorRequest />} />
-          <Route
-            exact
-            path="/AdminUserManagement"
-            element={<AdminUserManagement />}
-          />
-          <Route exact path="/VendorSearchPage" element={<SearchPage />} />
-          <Route
-            path="/VendorSearchPage/VendorName/:vendorName"
-            element={<VendorDetailpage />}
-          />
-          <Route
-            path="/VendorSearchPage/Category/:vendorCategory"
-            element={<CategoryDisplayPage />}
-          />
-          <Route
-            path="/LogisticsManagement/WeddingChecklist"
-            element={<WeddingChecklist />}
-          />
-          <Route exact path="/guestlist" element={<GuestList />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<FrontPage />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/sample" element={<Sample />} />
+                    <Route
+                        exact
+                        path="/vendor/request"
+                        element={<VendorRequest />}
+                    />
+                    <Route
+                        exact
+                        path="/AdminUserManagement"
+                        element={<AdminUserManagement />}
+                    />
+                    <Route
+                        exact
+                        path="/VendorSearchPage"
+                        element={<SearchPage />}
+                    />
+                    <Route
+                        path="/VendorSearchPage/VendorName/:vendorName"
+                        element={<VendorDetailpage />}
+                    />
+                    <Route
+                        path="/VendorSearchPage/Category/:vendorCategory"
+                        element={<CategoryDisplayPage />}
+                    />
+                    <Route
+                        path="/LogisticsManagement/WeddingChecklist"
+                        element={<WeddingChecklist />}
+                    />
+                    <Route exact path="/guestlist" element={<GuestList />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default EndPoints;
