0ed947807cfc3e29dfadf863788be822c3b8b455
michaell2002 <e0900731@u.nus.edu>
2023-04-07 18:24:11 +0800

    login merge conflict

--- wedding-planner/src/components/Login Page/Login.jsx
+++ wedding-planner/src/components/Login Page/Login.jsx
@@ -1,13 +1,13 @@
 import { Button } from "primereact/button";
 import React, { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PublicHeartyNavbar from "../HeartyNavbar/PublicHeartyNavbar";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import LoginAPI from "./LoginAPI";
 import { Dropdown } from "primereact/dropdown";
 import { LoginTokenContext } from "../../context/LoginTokenContext";
-
+import { EncodedJWTContext } from "../../context/EncodedJWTContext";
 //This is a sample of the component that is called by the Route component in EndPoints.jsx. This is almost like the page.
 //When you want to create a new page, just create a new folder in the components directory and add the components related to that page into that folder, before adding the Route component in EndPoints.jsx.
 
@@ -21,10 +21,8 @@ const userTypeItems = [
     { label: "Vendor", value: "VENDOR" },
 ];
 function Login() {
-    
+  
     const [token, setToken] = useContext(LoginTokenContext);
-    let navigate = useNavigate(); 
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [userType, setUserType] = useState("ADMIN");
@@ -41,23 +39,17 @@ function Login() {
         return successOrFailPromise.then((successOrFailString) => {
             if (successOrFailString == "success") {
                 // do some redirect to relevant page, but I think it should be done from the Router element
-                const indexPageString = "/";
-                navigate(indexPageString)
                 console.log("success");
                 return "success";
             } else {
                 // stay on login page
                 console.log("fail");
-                const loginPageString = "/login";
-                navigate(loginPageString)
                 return "fail";
             }
         });
     }
 
-    const doLogin = (ev) => {
-        console.log(ev);
-        ev.preventDefault();
+    function doLogin(userType, username, password, setToken) {
         switch (userType) {
             case "ADMIN":
                 handleLoginSuccessOrFail(
@@ -86,9 +78,8 @@ function Login() {
                 <Link to="/" className="flex justify-content-center noUnderline">
                     <Button
                         label="Login"
-                        type="submit"
                         style={{ backgroundColor: "#f561b0", border: "#f561b0" }}
-                        onClick={(e) => doLogin(e)}
+                        onClick={() => doLogin(userType, username, password, setToken)}
                     />{" "}
                 </Link>
                 <div className="flex justify-content-center px-5 mt-5">
@@ -106,48 +97,48 @@ function Login() {
             <PublicHeartyNavbar />
             <div className="flex justify-content-center pt-5">
                 <form onSubmit={doLogin}>
-                    <Card
-                        // title={title}
-                        footer={footer}
-                        style={{
-                            minWidth: "300px",
-                            maxWidth: "500px",
-                            minHeight: "500px",
+                <Card
+                    // title={title}
+                    footer={footer}
+                    style={{
+                        minWidth: "300px",
+                        maxWidth: "500px",
+                        minHeight: "500px",
                             maxHeight: "700px",
-                        }}
-                    >
+                    }} 
+                >
                         <h3 className="flex justify-content-center">Login</h3>
 
                         <div className="flex justify-content-center">
-                            {/* set flex grow to 1 */}
+                        {/* set flex grow to 1 */}
                             <p className="flex-1">Username: </p>
                             <InputText
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
                             />
-                        </div>
+                    </div>
                         <div className="flex justify-content-center">
                             <p className="flex-1">Password: </p>
                             <InputText
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
-                        </div>
+                    </div>
                         <div className="flex justify-content-center">
                             <p className="flex-1">User Type: </p>
-                            <Dropdown
-                                label="userType"
-                                options={userTypeItems}
-                                value={userType}
-                                onChange={(e) => {
-                                    setUserType(e.value);
-                                }}
-                            />
-                            {/* for debugging */}
-                            {/* <Button label="log userType val" onClick={() => console.log(userType)}/> */}
-                            {/* <Button label="log setToken val" onClick={() => console.log(setToken)} /> */}
-                        </div>
-                    </Card>
+                        <Dropdown
+                            label="userType"
+                            options={userTypeItems}
+                            value={userType}
+                            onChange={(e) => {
+                                setUserType(e.value);
+                            }}
+                        />
+                        {/* for debugging */}
+                        {/* <Button label="log userType val" onClick={() => console.log(userType)}/> */}
+                        {/* <Button label="log setToken val" onClick={() => console.log(setToken)} /> */}
+                    </div>
+                </Card>
                 </form>
             </div>
         </div>
