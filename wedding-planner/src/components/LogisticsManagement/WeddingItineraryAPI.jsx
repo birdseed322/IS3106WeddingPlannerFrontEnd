3d1b833c937df61aacd4d491215b879ac3f6a63a
Josephine <josephinesumargo88@gmail.com>
2023-04-07 20:55:14 +0800

    added APIs for wedding budget and wedding itinerary

---
+++ wedding-planner/src/components/LogisticsManagement/WeddingItineraryAPI.jsx
@@ -0,0 +1,42 @@
+const SERVER_PREFIX =
+    "http://localhost:8080/IS3106WeddingPlanner-war/webresources/LogisticsManagement/WeddingItinerary";
+
+const WeddingItineraryAPI = {
+    createNewItinerary(data, weddingProjectId) {
+        return fetch(`${SERVER_PREFIX}/create/${weddingProjectId}`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        });
+    },
+
+    getAllItineraries() {
+        return fetch(`${SERVER_PREFIX}/itineraries`);
+    },
+
+    getItinerary(weddingItineraryId) {
+        return fetch(`${SERVER_PREFIX}/${weddingItineraryId}`);
+    },
+
+    updateItinerary(data) {
+        return fetch(`${SERVER_PREFIX}/update/itinerary`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "PUT",
+            body: JSON.stringify(data),
+        });
+    },
+
+    deleteItinerary(weddingItineraryId) {
+        return fetch(`${SERVER_PREFIX}/${weddingItineraryId}`, {
+            method: "DELETE",
+        });
+    },
+};
+
+export default WeddingItineraryAPI;
